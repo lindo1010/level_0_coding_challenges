50a7c73589dcf47700b26a62d789e98c9b44d59f
lindo1010 <74527000+lindo1010@users.noreply.github.com>
2021-01-24 22:55:35 +0200

    Update Task0_5.java

--- Task0_5.java
+++ Task0_5.java
@@ -4,8 +4,8 @@ public class Task0_5 {
     public static void main(String[] args) {
         area_of_triangle(5,6,9);
     }
-    public static void area_of_triangle(int a, int b, int c) {
+    public static double area_of_triangle(int a, int b, int c) {
         double s = 0.5 * (a + b + c);
-        System.out.println("area is: " + s);
+        return s;
     }
 }
