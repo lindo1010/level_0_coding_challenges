d2011f8ed319a319c77f7706c5f671a97b2d785f
lindo1010 <74527000+lindo1010@users.noreply.github.com>
2021-01-25 11:28:57 +0200

    Update Task0_5.java

--- Task0_5.java
+++ Task0_5.java
@@ -1,11 +1,12 @@
 package com.company;
 
 public class Task0_5 {
-    public static void main(String[] args) {
-        area_of_triangle(5,6,9);
-    }
-    public static double area_of_triangle(int a, int b, int c) {
-        double s = 0.5 * (a + b + c);
-        return s;
-    }
+     public static void main(String[] args) {
+            area_of_triangle(5,4,3);
+        }
+        public static double area_of_triangle(int a, int b, int c) {
+            double s = 0.5 * (a + b + c);
+            double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+            return area;
+        }
 }
