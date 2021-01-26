38f2d9fee529c5a9ee46c5f18252e6f8b14ae2e9
lindo1010 <74527000+lindo1010@users.noreply.github.com>
2021-01-27 01:14:30 +0200

    Update Task0_10.java

--- Task0_10.java
+++ Task0_10.java
@@ -1,19 +1,24 @@
 package com.company;
 
 public class Task0_10 {
-public static void common_characters(String firstString, String secondString){
-        String common;
-        for (int x = 0; x < firstString.length(); x++){
-            for (int i = 0; i < secondString.length(); i++){
-                if (Character.toString(firstString.charAt(x)).equalsIgnoreCase(Character.toString(secondString.charAt(i)))){
-                    common = Character.toString(firstString.charAt(x));
-                    System.out.print(common);
+static void common_characters(String input1, String input2){
+
+        String common_strings;
+        String common_characters = "";
+
+        for (int i = 0; i < input1.length(); i++){
+            for (int z = 0; z < input2.length(); z++){
+                if (Character.toString(input1.charAt(i)).equalsIgnoreCase(Character.toString(input2.charAt(z)))){
+                    common_strings = Character.toString(input1.charAt(i));
+                    common_characters += common_strings + ",";
                 }
             }
         }
+        common_characters = common_characters.substring(0,common_characters.length()-1);
+        System.out.print("Common letters: " + common_characters);
     }
 
     public static void main(String[] args) {
-        common_characters("house","computers");
+        common_characters("computers","house");
     }
 }
