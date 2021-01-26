47e704790b3c305c64d286b287060736d8b4a1d6
lindo1010 <74527000+lindo1010@users.noreply.github.com>
2021-01-26 23:58:47 +0200

    Update Task0_9.java

--- Task0_9.java
+++ Task0_9.java
@@ -5,8 +5,6 @@ public class Task0_9 {
         print_vowels("Lindokuhle");
     }
     static void print_vowels(String randomString){
-        //used .replaceAll methode to replace non vowels with empty string and remain with vowels
-        // the "^" not of makes sure the values returned are the ones we want
         String vowelsInString = randomString.replaceAll("[^aeiouAEIOU]","");
         System.out.println(vowelsInString);
     }
