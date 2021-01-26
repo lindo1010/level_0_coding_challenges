3afa666e14a325a4b14d37c4c460df55cd69fe0f
lindo1010 <74527000+lindo1010@users.noreply.github.com>
2021-01-26 23:21:46 +0200

    Update Task0_3.java

--- Task0_3.java
+++ Task0_3.java
@@ -2,9 +2,9 @@ package com.company;
 
 public class Task0_3 {
     public static void main(String[] args) {
-        hello("Thabo");
+        hello("Tshepo");
     }
     public static void hello (String name){
-        System.out.println("hello" + name +"!");
+        System.out.println("Hello" + name +"!");
     }
 }
